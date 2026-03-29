053086209ae3cefeb2f209b90387d2e6a40d9a02
Kanchan Chaudhary <eunkanchan7@gmail.com>
2026-03-29 11:39:11 +0545

    to define the order of execution of each script

---
+++ realtime/main.py
@@ -0,0 +1,16 @@
+from realtime_capture import run_capture
+from feature_generation import run_extraction
+# from model_prediction import run_prediction
+
+def main():
+    # Step 1: Capture 120s of traffic
+    run_capture()
+
+    # Step 2: Generate features from that PCAP
+    run_extraction()
+
+    # Step 3: Run the AI model on the resulting CSV
+    # run_prediction()
+
+if __name__ == "__main__":
+    main()
