9d21c0b8e628b942237b64fff9d24a02794605cb
Kanchan Chaudhary <eunkanchan7@gmail.com>
2026-03-30 02:02:07 +0545

    incorporated prediction

--- realtime/main.py
+++ realtime/main.py
@@ -1,16 +1,16 @@
-from realtime_capture import run_capture
-from feature_generation import run_extraction
-# from model_prediction import run_prediction
+from realtime.realtime_capture import run_capture
+from realtime.feature_generation import run_feature_extraction
+from realtime.model_prediction import run_prediction
 
 def main():
     # Step 1: Capture 120s of traffic
     run_capture()
 
     # Step 2: Generate features from that PCAP
-    run_extraction()
+    run_feature_extraction()
 
     # Step 3: Run the AI model on the resulting CSV
-    # run_prediction()
+    run_prediction()
 
 if __name__ == "__main__":
     main()
